4290bf4074e0aa10da1e789b2486b0b7d65c7cb6
VictorNascimento14 <j_nascimento06@hotmail.com>
2025-03-14 19:49:45 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+  base: 'https://victornascimento14.github.io/eco-fisio-presence/',   // Add this line for GitHub Pages
+  server: { 
     host: "::",
     port: 8080,
   },
